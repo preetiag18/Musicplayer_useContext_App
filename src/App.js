07a7e018528bee5104ff48643cf362e7b4a06d80
preetiag18 <preetiag18@gmail.com>
2023-03-03 14:56:32 +0200

    use context to create music app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
-
+import heels from "./assets/heels.mp3";
+import door from "./assets/door.mp3";
+import raindrops from "./assets/raindrops.mp3";
+import { useState } from "react";
+import { TrackList } from "./components/TrackList";
+import { PlayerControls } from "./components/PlayerControls";
+import { MusicContext } from "./contexts/MusicContext";
 function App() {
-  return <div className="App"></div>;
+  const [state, setState] = useState({
+    audioPlayer: new Audio(),
+    tracks: [
+      {
+        name: "door music",
+        file: door,
+      },
+      {
+        name: "heels music",
+        file: heels,
+      },
+      {
+        name: "raindrops music",
+        file: raindrops,
+      },
+    ],
+    currentTrackIndex: null,
+    isPlaying: false,
+  });
+
+  return (
+    <MusicContext.Provider value={[state, setState]}>
+      <div className="App">
+        <TrackList />
+        <PlayerControls />
+      </div>
+    </MusicContext.Provider>
+  );
 }
 
 export default App;
---
+++ src/hooks/useMusicPlayer.js
@@ -0,0 +1,58 @@
+import { useContext } from "react";
+import { MusicContext } from "../contexts/MusicContext";
+
+const useMusicPlayer = () => {
+  const [state, setState] = useContext(MusicContext);
+
+  const togglePlay = () => {
+    if (state.isPlaying) {
+      setState({ ...state, isPlaying: false });
+      state.audioPlayer.pause();
+    } else {
+      setState({ ...state, isPlaying: true });
+      state.audioPlayer.play();
+    }
+  };
+
+  const playTrack = (index) => {
+    if (index === state.currentTrackIndex) {
+      togglePlay();
+    } else {
+      state.audioPlayer.pause();
+      state.audioPlayer = new Audio(state.tracks[index].file);
+      state.audioPlayer.play();
+      setState({ ...state, currentTrackIndex: index, isPlaying: true });
+    }
+  };
+
+  const playPreviousTrack = () => {
+    let newIndex = null;
+    state.currentTrackIndex === 0
+      ? (newIndex = state.tracks.length - 1)
+      : (newIndex = state.currentTrackIndex - 1);
+    playTrack(newIndex);
+  };
+
+  const playNextTrack = () => {
+    let newIndex = null;
+    state.currentTrackIndex === state.tracks.length - 1
+      ? (newIndex = 0)
+      : (newIndex = state.currentTrackIndex + 1);
+    playTrack(newIndex);
+  };
+
+  return {
+    playTrack,
+    togglePlay,
+    currentTrackIndex: state.currentTrackIndex,
+    currentTrackName:
+      state.currentTrackIndex !== null &&
+      state.tracks[state.currentTrackIndex].name,
+    trackList: state.tracks,
+    isPlaying: state.isPlaying,
+    playPreviousTrack,
+    playNextTrack,
+  };
+};
+
+export { useMusicPlayer };
